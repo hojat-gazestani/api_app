0f294106024f285fa1bcf9b02558e2bb7ab54ac2
hojat-gazestani <Hojat.Gazestani@gmail.com>
2023-08-16 13:42:33 +0330

    user authentication

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 
-from rest_framework import generics, permissions
+from rest_framework import generics#, permissions
 
 from .models import Post
+from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer
 
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (IsAuthorOrReadOnly)
+    #permission_classes = (permissions.IsAdminUser,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
